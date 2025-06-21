34e8febd55ad55480483addacbb86975a95ea863
RitikTiwary519 <202201519@daiict.ac.in>
2025-06-21 13:25:48 +0530

    spring security added

---
+++ src/main/java/net/engineeringdigest/journalApp/repository/userRepository.java
@@ -0,0 +1,15 @@
+package net.engineeringdigest.journalApp.repository;
+
+import net.engineeringdigest.journalApp.entity.JournalEntry;
+import net.engineeringdigest.journalApp.entity.User;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public interface userEntryRepository extends MongoRepository<User, ObjectId> {
+    User findybyusername(String usernmae);
+}
+
+//mongo repo is already presented interface that performs the basic crud opreration
+// controller ---> service ---> repository (which uses interface)
